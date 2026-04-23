a5db80453f1e405e4d512bc16bfe40638c6b69a9
mikael_azariants <mikazaryants@gmail.com>
2026-04-23 20:41:08 +0200

    debugging

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ from markdown_to_html_node import markdown_to_html_node
 from extract_title import extract_title
 from copy_dir import copy_dir
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as from_file:
         contents_from = from_file.read()
@@ -12,7 +12,7 @@ def generate_page(from_path, template_path, dest_path):
     
     html_string = markdown_to_html_node(contents_from).to_html()
     page_title = extract_title(contents_from)
-    new_contents = contents_template.replace("{{ Title }}", page_title).replace("{{ Content }}", html_string)
+    new_contents = contents_template.replace("{{ Title }}", page_title).replace("{{ Content }}", html_string).replace('src="/', f'src="{basepath}').replace('href=/"', f'href="{basepath}')
     
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as dest_file:
--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from copy_dir import copy_dir
 from generate_pages_recursive import generate_pages_recursive
+import sys
 def main(src, dst):
-    copy_dir(src, dst)
-    generate_pages_recursive("./content", "template.html", "./public")    
-main("static", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    
+    copy_dir("." + basepath+src, "." + basepath+dst)
+    generate_pages_recursive("." + basepath + "content", "." + basepath + "template.html", "." + basepath + "docs", basepath)    
+    
+main("static", "docs")
 
